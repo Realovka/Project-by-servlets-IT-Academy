556d65c6e9ee874830ea522590b43848cf1bdc40
realovka <realovka@mail.ru>
2021-01-19 11:57:15 +0300

    add students and themes to trainer

--- app/src/main/java/by/realovka/web/app/filter/AuthFilter.java
+++ app/src/main/java/by/realovka/web/app/filter/AuthFilter.java
@@ -6,8 +6,6 @@ import by.realovka.web.service.UserService;
 import by.realovka.web.service.UserServiceImpl;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
