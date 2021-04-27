ac797b1b6c9eaf5446a4f71f2d89aa291bab8cbc
realovka <realovka@mail.ru>
2021-04-27 21:10:38 +0300

    refactor config

--- app/src/main/java/by/realovka/web/app/config/ApplicationConfig.java
+++ app/src/main/java/by/realovka/web/app/config/ApplicationConfig.java
@@ -1,32 +1,27 @@
 package by.realovka.web.app.config;
 
-//import by.realovka.web.app.interceptor.AuthInterceptor;
-import lombok.AllArgsConstructor;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.*;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @ComponentScan({"by.realovka.web.app", "by.realovka.web.dao", "by.realovka.web.service"})
 @EnableWebMvc
 @EnableAspectJAutoProxy
 @PropertySource("classpath:application.properties")
-@EnableJpaRepositories(basePackages = "by.realovka.web.dao.repository")
+@EnableJpaRepositories(basePackages = "by.realovka.web.dao.repository", entityManagerFactoryRef = "factory", transactionManagerRef = "jpaProperties")
 public class ApplicationConfig {
 
     @Value("${driver}")
@@ -48,10 +43,10 @@ public class ApplicationConfig {
         return driverManagerDataSource;
     }
 
-//    @Bean
-//    public JdbcTemplate jdbcTemplate() {
-//        return new JdbcTemplate(dataSource());
-//    }
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
+    }
 
 
     @Bean
@@ -62,29 +57,32 @@ public class ApplicationConfig {
         return internalResourceViewResolver;
     }
 
-//    @Bean
-//    public LocalSessionFactoryBean sessionFactory() {
-//        LocalSessionFactoryBean localSessionFactoryBean = new LocalSessionFactoryBean();
-//        localSessionFactoryBean.setDataSource(dataSource());
-//        localSessionFactoryBean.setPackagesToScan("by.realovka.web.dao.model");
-//        localSessionFactoryBean.setHibernateProperties(hibProps());
-//        return localSessionFactoryBean;
-//    }
-//
-//    @Bean
-//    public PlatformTransactionManager transactionManager() {
-//        HibernateTransactionManager hibernateTransactionManager = new HibernateTransactionManager();
-//        hibernateTransactionManager.setSessionFactory(sessionFactory().getObject());
-//        return hibernateTransactionManager;
-//    }
-//
-//    @Bean
-//    public Properties hibProps() {
-//        Properties properties = new Properties();
-//        properties.setProperty("hibernate.hbm2ddl.auto", "none");
-//        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
-//        properties.setProperty("hibernate.show_sql", "true");
-//        return properties;
-//    }
+    @Bean
+    public LocalContainerEntityManagerFactoryBean factory() {
+        LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+        emf.setDataSource(dataSource());
+        emf.setPersistenceUnitName("jpa-unit");
+        emf.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        emf.setPackagesToScan("by.realovka.web.model");
+        emf.setJpaProperties(jpaProperties());
+        return emf;
+    }
+
+    @Bean
+    public Properties jpaProperties() {
+        Properties props = new Properties();
+        props.setProperty("connection.pool_size", "20");
+        props.setProperty("dialect", "org.hibernate.dialect.PostgreSQL10Dialect");
+        props.setProperty("show_sql", "true");
+        props.setProperty("current_session_context_class", "thread");
+        props.setProperty("hbm2ddl.auto", "none");
+        props.setProperty("hibernate.dbcp.initialSize", "5");
+        props.setProperty("hibernate.dbcp.maxTotal", "20");
+        props.setProperty("hibernate.dbcp.maxIdle", "10");
+        props.setProperty("hibernate.dbcp.minIdle", "5");
+        props.setProperty("hibernate.dbcp.maxWaitMillis", "-1");
+        props.setProperty("hibernate.enable_lazy_load_no_trans", "true");
+        return props;
+    }
 
 }
